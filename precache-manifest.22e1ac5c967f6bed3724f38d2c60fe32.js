dcdd66985cbd65aa660de45e872f7eb43a6876e4
devolasvegas <devondaviau@yahoo.ca>
2019-03-18 22:22:08 -0400

    Updates

---
+++ precache-manifest.22e1ac5c967f6bed3724f38d2c60fe32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "150a246566c57de60a30",
+    "url": "/react-stopwatch/static/css/main.981d0e59.chunk.css"
+  },
+  {
+    "revision": "150a246566c57de60a30",
+    "url": "/react-stopwatch/static/js/main.86c41dae.chunk.js"
+  },
+  {
+    "revision": "1e6f92fa7bd110bc0e8e",
+    "url": "/react-stopwatch/static/js/runtime~main.b7a26dfe.js"
+  },
+  {
+    "revision": "8e5c89340b11e04c4987",
+    "url": "/react-stopwatch/static/js/2.fa4c2f23.chunk.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/react-stopwatch/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "377263a3ff11bd5698e68b1a082ba927",
+    "url": "/react-stopwatch/index.html"
+  }
+];
