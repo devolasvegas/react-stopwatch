efa46533511d8eb7a2383f047527e554c3315b17
devolasvegas <devondaviau@yahoo.ca>
2019-03-18 23:06:47 -0400

    add vscode config folder to ignore file, start styling component

--- src/Stopwatch.js
+++ src/Stopwatch.js
@@ -4,29 +4,82 @@ import styled from 'styled-components'
 import logo from './logo.svg';
 import './App.css';
 
-const StyledStopwatch = styled.article`
-  background: #000000;
+const StyledStopwatch = styled.main`
+  text-align: center;
+
+  .App-logo {
+    animation: App-logo-spin infinite 20s linear;
+    height: 40vmin;
+    pointer-events: none;
+  }
+
+  .App-header {
+    background-color: #282c34;
+    min-height: 100vh;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    font-size: calc(10px + 2vmin);
+    color: white;
+  }
+
+  .App-link {
+    color: #61dafb;
+  }
+
+  .button-container {
+    width: 900px;
+    max-width: 90%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex-wrap: wrap;
+
+    button {
+      color: #61dafb;
+      font-size: 1.25rem;
+      margin: 0.5em;
+      padding: 0.25em 5%;
+      background: transparent;
+      border: 3px solid #61dafb;
+      border-radius: 3px;
+    }
+  }
+
+
+  @keyframes App-logo-spin {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
 `
 
 class Stopwatch extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
+      <StyledStopwatch>
+        <article className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
           </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+          <div className="button-container">
+            <button className="start-button">
+              Start
+            </button>
+            <button className="split-button">
+              Split
+            </button>
+            <button className="reset-button">
+              Reset
+            </button>
+          </div>
+        </article>
+      </StyledStopwatch>
     );
   }
 }
